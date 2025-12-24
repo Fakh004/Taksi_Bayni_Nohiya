afe3b4c0cbe89185bb6c9f6ca8a72c1e9c93376b
fakh <fakhridin008@gmail.com>
2025-12-24 16:43:31 +0300

    register te,plates login base

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 from .models import *
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
@@ -8,26 +6,26 @@ from django.contrib.auth import login, authenticate, logout
 
 def register_view(request):
     if request.method == "GET":
-        return render(request, "register.html")
+        return render(request, "accounts/register.html")
     elif request.method == "POST":
         username = request.POST.get("username", None)
         email = request.POST.get("email", None)
         password = request.POST.get("password", None)
         confirm = request.POST.get("confirm_password", None)
         if not username or not password or not email:
-            return render(request, "register.html", {
+            return render(request, "accounts/register.html", {
                 "username": username,
                 "email": email,
                 "error": "All fields are required."})
         if password != confirm:
-            return render(request, "register.html", {
+            return render(request, "accounts/register.html", {
                 "username": username,
                 "email": email,
                 "error": "Passwords do not match."})
         hash_password = make_password(password)
         user= User.objects.filter(username=username).first()
         if user:
-            return render(request, "register.html", {
+            return render(request, "accounts/register.html", {
                 "username": username,
                 "email": email,
                 "error": "Username already exists."})
@@ -42,21 +40,21 @@ def register_view(request):
 
 def login_view(request):
     if request.method == "GET":
-        return render(request, "login.html")
+        return render(request, "accounts/login.html")
     elif request.method == "POST":
         username = request.POST.get("username", None)
         password = request.POST.get("password", None)
         user = authenticate(username=username, password=password)
         if user:
             login(request, user)
-            return redirect("post_list")
+            return redirect("home")
         else:
-            return render(request, "login.html", {"error": "Invalid credentials"})
+            return render(request, "accounts/login.html", {"error": "Invalid credentials"})
 
 
 def logout_view(request):
     if request.method == "GET":
-        return render(request, "logout.html")
+        return render(request, "accounts/logout.html")
     elif request.method == "POST":
         logout(request)
-        return redirect("post_list")
+        return redirect("home")
