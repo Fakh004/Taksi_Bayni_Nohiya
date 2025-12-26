652c03732ad13e300721116e4b8fb127470fe76a
fakh <fakhridin008@gmail.com>
2025-12-26 19:44:20 +0300

    urls dobavit

--- server/urls.py
+++ server/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('drivers/', include('drivers.urls')),
+    path('rides/', include('rides.urls')),
 ]
