a5a166ccf77788c6ad5f068297103f76357e447c
fakh <fakhridin008@gmail.com>
2025-12-27 04:08:25 +0300

    HTMLHO, va CastomUser, Settings

--- accounts/views.py
+++ accounts/views.py
@@ -1,60 +1,56 @@
-from django.shortcuts import render, redirect
-from .models import *
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render,redirect,HttpResponse
+from .models import CustomUser
+from django.contrib.auth import login,logout,authenticate
+
 
 def register_view(request):
     if request.method == "GET":
-        return render(request, "accounts/register.html")
+        return render(request,'accounts/register.html')
     elif request.method == "POST":
-        username = request.POST.get("username", None)
-        email = request.POST.get("email", None)
-        password = request.POST.get("password", None)
-        confirm = request.POST.get("confirm_password", None)
-        if not username or not password or not email:
-            return render(request, "accounts/register.html", {
-                "username": username,
-                "email": email,
-                "error": "All fields are required."})
+        username = request.POST.get('username',None)
+        email = request.POST.get('email',None)
+        password = request.POST.get('password',None)
+        confirm = request.POST.get('confirm',None)
+
+        if not username or not email or not password:
+            return render(request,'accounts/register.html',context={'username':username,"email":email,'error':'All fields are required!'})
+        
         if password != confirm:
-            return render(request, "accounts/register.html", {
-                "username": username,
-                "email": email,
-                "error": "Passwords do not match."})
-        hash_password = make_password(password)
-        user= User.objects.filter(username=username).first()
-        if user:
-            return render(request, "accounts/register.html", {
-                "username": username,
-                "email": email,
-                "error": "Username already exists."})
-        user = User(
-            username=username,
-            email=email,
-            password=hash_password
-        )
-        user.save()
-        return redirect("login")
+            return render(request,'accounts/register.html',context={'username':username,'error':"Passwords don't match!"})
+        
+        CustomUser.objects.create_user(username=username,email=email,password=password)
+
+
+        return HttpResponse('Logined')
+
 
 
 def login_view(request):
     if request.method == "GET":
-        return render(request, "accounts/login.html")
+        return render(request, 'accounts/login.html')
+    
     elif request.method == "POST":
-        username = request.POST.get("username", None)
-        password = request.POST.get("password", None)
-        user = authenticate(username=username, password=password)
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        if not username or not password:
+            return render(request, 'accounts/login.html', context={
+                'username': username,
+                'error': 'All fields are required!'
+            })
+        
+        user = authenticate(request, username=username, password=password)
+
         if user:
             login(request, user)
-            return redirect("home")
-        else:
-            return render(request, "accounts/login.html", {"error": "Invalid credentials"})
+            return redirect("driver_list")
+
+        return render(request, 'accounts/login.html', context={
+            'username': username,
+            'error': "Such user does not exist!"
+        })
+    
 
 
-def logout_view(request):
-    if request.method == "GET":
-        return render(request, "accounts/logout.html")
-    elif request.method == "POST":
-        logout(request)
-        return redirect("home")
+def logout_confirm(request):
+    return render(request, "accounts/logout_confirm.html")
